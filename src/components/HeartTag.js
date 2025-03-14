22296df13fe2bb99a53cab8968818516d88d5781
silverraining <www.stella.choi@gmail.com>
2025-03-14 17:17:53 +0900

    🧐 Fix: restore register dir. case sensitivity issue in register

--- src/components/HeartTag.js
+++ src/components/HeartTag.js
@@ -4,20 +4,19 @@ import Image from "next/image";
 
 export default function HeartTag({ product, initialFavoriteCount }) {
   const [favoriteCount, setFavoriteCount] = useState(initialFavoriteCount);
-  const [isFavorite, setIsFavorite] = useState(product.isFavorite); // 상태로 관리
+  const [isFavorite, setIsFavorite] = useState(product.isFavorite);
   const handleLike = async () => {
     try {
       const response = await instance.post(`/products/${product.id}/favorite`);
       if (response.status === 200) {
         setFavoriteCount((prev) => prev + 1);
-        setIsFavorite(true); // 좋아요 상태 업데이트
+        setIsFavorite(true);
       }
     } catch (error) {
       if (
         error.response &&
         error.response.data.message === "이미 찜한 상품입니다."
       ) {
-        // 이미 찜한 상품일 경우 처리
         console.error(error.response.data.message); // "이미 찜한 상품입니다."
       } else {
         console.error("좋아요 추가 실패:", error);
@@ -39,7 +38,6 @@ export default function HeartTag({ product, initialFavoriteCount }) {
         error.response &&
         error.response.data.message === "찜하지 않은 상품입니다."
       ) {
-        // 찜하지 않은 상품일 경우 처리
         console.error(error.response.data.message); // "찜하지 않은 상품입니다."
       } else {
         console.error("좋아요 취소 실패:", error);
@@ -62,7 +60,6 @@ export default function HeartTag({ product, initialFavoriteCount }) {
       onClick={toggleLike}
       className="flex items-center h-full bg-[#fcfcfc] border px-3 py-2 cursor-pointer rounded-full"
     >
-      {/* 좋아요 상태에 따라 이미지 변경 */}
       <Image
         src={isFavorite ? "/ic_favorite.svg" : "/ic_heart.svg"}
         width={24}
